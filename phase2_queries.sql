232ac705304cbf8d4ec3161eba56fbe91d255d5f
mahdie <davoudzademahdie@gmail.com>
2026-06-17 01:55:52 +0330

    queries about reservation

--- phase2_queries.sql
+++ phase2_queries.sql
@@ -21,3 +21,63 @@ on r.user_id = u.user_id
 inner join Payment p
 on r.reserve_id = p.reservation_id
 group by u.user_id, MONTH(p.paied_at), YEAR(p.paied_at), u.first_name,u.last_name;
+
+
+-- 4
+select u.first_name , u.last_name, u.email, u.phone, u.profile_image, u.city_id
+from User u
+inner join Reserve r 
+on r.user_id = u.user_id
+order by r.reserve_at desc 
+limit 1;
+
+-- 5
+select u.phone as phone, u.email as email 
+from User u
+inner join Reserve r 
+on r.user_id = u.user_id
+inner join Payment p 
+on r.reserve_id = p.reservation_id
+group by u.user_id
+having sum(p.amount) > (
+    select avg(total)
+    from (
+        select sum(p2.amount) as total
+        from Reserve r2
+        inner join Payment p2
+        on r2.reserve_id = p2.reservation_id
+        group by r2.user_id
+    )t 
+)
+
+
+-- 6
+select u.first_name as name, u.last_name as last_name , count(r.reserve_id) as total
+from User u 
+inner join Reserve r
+on u.user_id = r.user_id
+where r.reserve_at >= now() - interval 7 day 
+group by 
+u.user_id
+order by total desc
+limit 3;
+
+-- 7
+select DISTINCT c.name as city , u.user_id, u.first_name, u.last_name
+from City c 
+inner join Venue v
+on c.city_id = v.city_id
+inner join Stadium s 
+on v.venue_id = s.venue_id
+inner join Match m 
+on s.stadium_id = m.stadium_id
+inner join Ticket t 
+on m.match_id = t.match_id
+inner join Reserve r 
+on t.ticket_id = r.ticket_id
+inner join User u 
+on u.user_id = r.user_id
+where u.created_at =(
+    select min(u2.created_at)
+    from User u2
+);
